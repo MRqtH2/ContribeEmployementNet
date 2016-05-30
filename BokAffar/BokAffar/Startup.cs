b169bc1946031e87aec5b4229b996af20baa4188
Per Frost <per.c.frost@gmail.com>
2016-05-31 00:44:09 +0200

    Initial check-in

---
+++ BokAffar/BokAffar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BokAffar.Startup))]
+namespace BokAffar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
