c25bba0a7ba9d9b3ec44d1bbe198268ec67cff71
Per Frost <per.c.frost@gmail.com>
2016-05-31 22:53:29 +0200

    Added file for IBook Interface.

---
+++ BokAffar/BokAffar/Interfaces/IBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BokAffar.Interfaces
+{
+    public interface IBook
+    {
+        string Title { get; }
+        string Author { get; }
+        decimal Price { get; }
+    }
+}
